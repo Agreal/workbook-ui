cc865217db2fcfca7e8c6a0c2b462b3cdaf7c1ed
Qin Xiaoqiang <xqqin@thoughtworks.com>
2015-03-01 17:28:39 +0800

    update rotate style

--- colorful-clock/scripts/main.js
+++ colorful-clock/scripts/main.js
@@ -1,1 +1,13 @@
-console.log('Hello!');
+$(function () {
+    var counter = 0;
+    $('#btn').click(function () {
+        if (counter%2 === 0) {
+            console.log(counter);
+            $('#iframe').attr('src', $('#iframe').data('src'));
+        } else {
+            $('#iframe').attr('src', '');
+            console.log(counter);
+        }
+        counter++;
+    });
+});
